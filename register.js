9137b464418fca3bf9997643c19fb576e4586acd
Guilherme IA <Iazzetta@users.noreply.github.com>
2025-02-05 22:43:55 -0300

    publish: register.js

--- register.js
+++ register.js
@@ -13,22 +13,22 @@ document.getElementById('register-form').addEventListener('submit', function(eve
     .then(data => {
         if (data.status) {
             // Automatically login the user
-            fetch('http://localhost:8000/functions/yt2/login_user', {
+            return fetch('http://localhost:8000/functions/yt2/login_user', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ 'username': username, 'password': password })
-            })
-            .then(response => response.json())
-            .then(loginData => {
-                if (loginData.status) {
-                    localStorage.setItem('auth_token', loginData.auth_token);
-                    window.location.href = 'payment.html';
-                } else {
-                    alert(loginData.error);
-                }
             });
         } else {
-            alert(data.error);
+            throw new Error(data.error);
+        }
+    })
+    .then(response => response.json())
+    .then(loginData => {
+        if (loginData.status) {
+            localStorage.setItem('auth_token', loginData.auth_token);
+            window.location.href = 'payment.html';
+        } else {
+            alert(loginData.error);
         }
     })
     .catch(error => console.error('Error:', error));
